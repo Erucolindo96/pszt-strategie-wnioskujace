bfcfc33326d1fc6e7b28ae2d5eb94068479352d5
dram <twiwala@dram.pl>
2017-12-30 02:33:10 +0100

    Tabele w klauzuli

--- src/mainWindow/Controller.java
+++ src/mainWindow/Controller.java
@@ -6,13 +6,16 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextFlow;
 import javafx.stage.FileChooser;
 import Inference.strategy.*;
+import javafx.scene.layout.RowConstraints;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -31,14 +34,19 @@ public class Controller implements Initializable, Observer {
     MenuBar menuBar;
     @FXML
     TextFlow textFlow;
+    @FXML
+    GridPane clausulesTable;
+    int clausulesTableRows;
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
         ConsoleLogger.INSTANCE.setTextFlow(textFlow);
         ConsoleLogger.INSTANCE.LOG(LEVEL.ERROR, "Hej");
         ConsoleLogger.INSTANCE.LOG(LEVEL.INFO,"Heeej");
+        addClausulesHeader();
 
-
+        addClausuleStep("a", "b", "c", "d");
+        addClausuleStep("a", "b", "x");
     }
 
 
@@ -76,6 +84,33 @@ public class Controller implements Initializable, Observer {
     {
         System.out.println("Jestem obserwatorem");// TODO jak Tomasz zrobi GUI to tu bedzie obsluga tego co wywnioskowalo InferenceMachine
     }
+    public void addClausulesHeader(){
+        Font headerFont = new Font("Arial", 25);
+        RowConstraints constraints = new RowConstraints();
+        constraints.setPrefHeight(40);
+        Label clausuleNumber = new Label("#");
+        Label clausulesTitle = new Label("Klauzule");
+        clausuleNumber.setFont(headerFont);
+        clausulesTitle.setFont(headerFont);
+        GridPane.setHgrow(clausuleNumber, Priority.ALWAYS);
+        GridPane.setHgrow(clausulesTitle, Priority.ALWAYS);
+        clausulesTable.add(clausuleNumber,0, clausulesTableRows);
+        clausulesTable.add(clausulesTitle,1, clausulesTableRows);
 
+        clausulesTable.getRowConstraints().add(constraints);
+
+        ++clausulesTableRows;
+    }
+    public void addClausuleStep(String ...clausules){
+        RowConstraints constraints = new RowConstraints();
+        constraints.setPrefHeight(20);
+        for(int i=0; i < clausules.length; i++){
+            Node label = new Label(clausules[i]);
+            clausulesTable.add(label,i, clausulesTableRows);
+            GridPane.setHgrow(label, Priority.ALWAYS);
+        }
+        clausulesTable.getRowConstraints().add(constraints);
+        ++clausulesTableRows;
+    }
 
 }
--- src/mainWindow/Main.java
+++ src/mainWindow/Main.java
@@ -1,22 +1,27 @@
 package mainWindow;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    @FXML
+    TableView mainTable;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
         Parent root = (Parent) loader.load();
         primaryStage.setTitle("Pszt - strategie wnioskujace");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
